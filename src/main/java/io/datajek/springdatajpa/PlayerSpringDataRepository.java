911b9a37c7a9cd4620ca3c4474fbf14b9d28cd71
0101coding <mayowadpastevans@yahoo.co.uk>
2024-07-22 06:43:26 +0100

    ADded Method in interface

--- src/main/java/io/datajek/springdatajpa/PlayerSpringDataRepository.java
+++ src/main/java/io/datajek/springdatajpa/PlayerSpringDataRepository.java
@@ -1,7 +1,9 @@
 package io.datajek.springdatajpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerSpringDataRepository extends JpaRepository<Player, Integer> {
-
+    public List<Player> findByNationality(String nationality);
 }
